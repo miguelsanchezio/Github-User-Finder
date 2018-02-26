3b958b0903f2a47b88075a6d67a07ffb096602a7
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-26 11:23:14 -0800

    Repos now display properly

--- js/github.js
+++ js/github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = '02a65db1723204421745';
         this.client_secret = 'a65c3905ff1fbd2680cde8ec6101d788684102cb';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
--- js/ui.js
+++ js/ui.js
@@ -33,6 +33,31 @@ class UI {
         `;
     }
 
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach(repo => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-warning">Watchers: ${repo.watchers_count}</span>
+                            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+        // Output repos
+        document.getElementById('repos').innerHTML = output;
+    }
+
     // Clear profile from UI
     clearProfile() {
         this.profile.innerHTML = '';
